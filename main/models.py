191a0adb4d09157d674cdf9b31f2242d4239e746
Laurence2013 <djroa1985@gmail.com>
2018-12-30 16:05:58 +0000

    2nd commit

--- main/models.py
+++ main/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+class Category_table(models.Model):
+    CATEGORY_TYPE = (
+            ('Astrophysicist','Astrophysicist'),
+            ('Civil Engineering','Civil Engineering'),
+            ('Politics','Politics'),
+    )
+    category = models.CharField(max_length = 100, choices = CATEGORY_TYPE)
+    date_updated = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.category
+
+    class Meta:
+        verbose_name_plural = 'Category'
+
+class Ask_A_Question_table(models.Model):
+    question = models.TextField()
+    date_updated = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        verbose_name_plural = 'Ask a question'
+
+class Vote_A_table(models.Model):
+    vote = models.TextField()
+    total_votes = models.IntegerField()
+    date_updated = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.vote
+
+    class Meta:
+        verbose_name_plural = 'Vote A'
+
+class Vote_B_table(models.Model):
+    vote = models.TextField()
+    total_votes = models.IntegerField()
+    date_updated = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.vote
+
+    class Meta:
+        verbose_name_plural = 'Vote B'
+
+class Vote_C_table(models.Model):
+    vote = models.TextField()
+    total_votes = models.IntegerField()
+    date_updated = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.vote
+
+    class Meta:
+        verbose_name_plural = 'Vote C'
+
+class Answer_table(models.Model):
+    answer = models.TextField()
+    date_updated = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.answer
+
+    class Meta:
+        verbose_name_plural = 'Answer'
+    
