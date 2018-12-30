f11f5071ee04dacb125e2b821d1aecc80cf773f5
Laurence2013 <djroa1985@gmail.com>
2018-12-30 16:16:56 +0000

    5th commit

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from main.models import *
 
-# Register your models here.
+admin.site.register(Category_table)
+admin.site.register(Ask_A_Question_table)
+admin.site.register(Vote_A_table)
+admin.site.register(Vote_B_table)
+admin.site.register(Vote_C_table)
+admin.site.register(Answer_table)
+
