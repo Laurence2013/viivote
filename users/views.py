37ebfeee318dfa9533d5433fabe2203fd4e4748c
Laurence2013 <djroa1985@gmail.com>
2018-12-30 19:52:44 +0000

    1st 1 commit

--- users/views.py
+++ users/views.py
@@ -1,17 +1,30 @@
 from django.views.generic import View
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import HttpResponse, render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 class Register(View):
     def get(self, request, *args, **kwargs):
         register = UserCreationForm() 
         return render(request, 'register.html', {'register': register})
 
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            register = UserCreationForm(request.POST)
+            if register.is_valid():
+                username = register.cleaned_data.get('username')
+                messages.success(request, f'Account created for {username}!')
+                return redirect('main')
+        else:
+            register = UserCreationForm()
+        messages.warning(request, f'Error, check your username or password')
+        return render(request, 'register.html', {'register': register})
+
 class Login(View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse('Hello from Login')
+        return render(request, 'login.html', {})
 
 class Logout(View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse('Hello from Logout')
+        return render(request, 'logout.html', {})
 
