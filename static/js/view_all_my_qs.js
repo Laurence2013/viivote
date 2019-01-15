a0f8bc5637b2fb7acb79103c68ee77dc9beb5f2c
Laurence2013 <djroa1985@gmail.com>
2019-01-15 15:52:56 +0000

    7th 5-7s commit

--- static/js/view_all_my_qs.js
+++ static/js/view_all_my_qs.js
@@ -3,12 +3,19 @@
   http.onreadystatechange = function(){
     if(http.readyState == 4 && http.status == 200){
       let results = JSON.parse(http.responseText);
+      let html = '';
 
+      html += '<ul>';
       Object.values(results).forEach(function(result){
-        console.log(result.question);
+        console.log(result);
+        html += '<li>'+ result.question.question+' - <small>date added: </small>'+ result.question.date_updated.slice(0, -14)+'</li>';
+        html +='<li><b>Vote A: </b>'+ result.vote_a.vote +'</li>';
+        html +='<li><b>Vote B: </b>'+ result.vote_b.vote +'</li>';
+        html +='<li><b>Vote C: </b>'+ result.vote_c.vote +'</li>';
+        html += '<hr>';
       });
-
-
+      html += '</ul>';
+      document.getElementById('get_all_questions').innerHTML = html;
     }
   }
   http.open('GET', 'get_all_my_questions', true);
