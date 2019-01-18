5b5bfa335c520ea94c6e0908fa9d36beab6a5346
Laurence2013 <djroa1985@gmail.com>
2019-01-18 14:47:41 +0000

    6th 5-6s commit

--- main/views.py
+++ main/views.py
@@ -341,7 +341,7 @@ class Main(View):
     
     def __get_questions(self, user_id):
         votes_list = []
-        get_qs = Ask_A_Question_table.objects.values_list('id')
+        get_qs = Ask_A_Question_table.objects.values_list('id').order_by('-date_updated')
         qs = [get_qs[qs] for qs in range(0,len(get_qs))]
         for ques_vote_q in qs:
             vote_id = Questions_Votes_table.objects.filter(votes_id_id = ques_vote_q[0]).values_list('votes_id_id')
@@ -361,13 +361,13 @@ class Main(View):
             get_qs_vote = User_Questions_Votes_Answers_table.objects.filter(question_id_id = get_qs).values('question_id_id','answer_id_id','user_id_id')
             try:
                 for vote in range(0,len(get_qs_vote)):
-                    get_anss = Answer_table.objects.filter(id = get_qs_vote[vote].get('answer_id_id')).values('id','answer')[0]
+                    get_anss = Answer_table.objects.filter(id = get_qs_vote[vote].get('answer_id_id')).values('id','answer','date_updated')[0]
                     qs_anss = get_qs_vote[0].get('question_id_id'), get_anss
                     qs_answers.append(qs_anss)
             except IndexError as e:
                 pass #print(e) save to log eventually
             
-            get_q = Ask_A_Question_table.objects.filter(id = get_qs).values_list('id','question') 
+            get_q = Ask_A_Question_table.objects.filter(id = get_qs).values_list('id','question','date_updated') 
             for v in range(0,len(get_q)):
                 get_vote_ids = Votes_table.objects.filter(id = qv).values_list('vote_a_id','vote_b_id','vote_c_id')
                 vote_a = Vote_A_table.objects.filter(id = get_vote_ids[0][0]).values_list('id','vote')[0]
@@ -398,6 +398,7 @@ class Main(View):
             'has_voted': T_F,
             'question_id': get_q[0][0],
             'question': get_q[0][1],
+            'date': get_q[0][2],
             'vote_a': con_vote_a,
             'vote_b': con_vote_b,
             'vote_c': con_vote_c,
@@ -415,6 +416,7 @@ class Main(View):
                 context_ans = {
                     'question_id': get_q[0][0],
                     'answer_id': qs_answers[ans][1].get('id'),
+                    'date_updated': qs_answers[ans][1].get('date_updated'),
                     'answer': qs_answers[ans][1].get('answer'),
                     'user_id': username.get('id'),
                     'username': username.get('username'),
--- static/js/all_qs.js
+++ static/js/all_qs.js
@@ -23,6 +23,14 @@
   let bookmark = 'bookmark';
   let del_ete = 'delete'
 
+  let new_date_format = function(datee){
+    let date_split = datee.split("-");
+    let year = date_split[0];
+    let month = date_split[1];
+    let day = date_split[2];
+    return day + '/' + month + '/' + year;
+  }; 
+
   http.onreadystatechange = function(){
     if(http.readyState == 4 && http.status == 200){
       let results = JSON.parse(http.responseText);
@@ -30,19 +38,21 @@
       let html = '';
 
       Object.values(results).forEach(function(qs){
+        let date_updatedd = qs.date.slice(0, -14);
         html += '<ul>';
         html += '<div class="results_vote">';
-        html += '<li><b>Question: </b>'+ qs.question +' <small>asked by</small> '+ qs.asked_by +' | <small><a href="'+ base_url + bookmark + '/' +qs.question_id +'">Bookmark</a></small></li>';
+        html += '<li><b>Question: </b>'+ qs.question +' <small>asked by</small> '+ qs.asked_by +' | <small>'+ new_date_format(date_updatedd) +'</small> | <small><a href="'+ base_url + bookmark + '/' + qs.question_id +'">Bookmark</a></small></li>';
         html += '<li><b>Vote A: </b><input type="radio" name="'+ qs.vote_a.questions_vote_id +'" value="'+ qs.vote_a.id +'">'+ qs.vote_a.vote +'</li>';
         html += '<li><b>Vote B: </b><input type="radio" name="'+ qs.vote_b.questions_vote_id +'" value="'+ qs.vote_b.id +'">'+ qs.vote_b.vote +'</li>';
         html += '<li><b>Vote C: </b><input type="radio" name="'+ qs.vote_c.questions_vote_id +'" value="'+ qs.vote_c.id +'">'+ qs.vote_c.vote +'</li>';
         if(qs.answers.length > 0){
           html += '<b>All Answers</b>';
           Object.values(qs.answers).forEach(function(qss){
+            let answer_date = qss.date_updated.slice(0, -14);
             if(qs.user_id === qss.user_id){
-              html += '<li>'+ qss.answer +' <small>answered by</small> '+ qss.username +' -- <small><a href="'+ base_url + edit + '/' + qss.answer_id +'">Edit</a> | <a href="'+ base_url + del_ete + '/'+ qss.answer_id +'">Delete</a></small></li>';
+              html += '<li>'+ qss.answer +' <small>answered by</small> '+ qss.username +' | '+ new_date_format(answer_date) +' -- <small><a href="'+ base_url + edit + '/' + qss.answer_id +'">Edit</a> | <a href="'+ base_url + del_ete + '/'+ qss.answer_id +'">Delete</a></small></li>';
             }else{
-              html += '<li>'+ qss.answer +' <small>answered by</small> '+ qss.username +'</li>';
+              html += '<li>'+ qss.answer +' <small>answered by</small> '+ qss.username +' | '+ new_date_format(answer_date) +'</li>';
             }
           });
         }
