31e831f29a42ef13bb79ccab7f1eaf6667b378bd
Laurence2013 <djroa1985@gmail.com>
2019-01-27 19:35:04 +0000

    1st 7-1s commit

--- main/tests/sampleTest.py
+++ main/tests/sampleTest.py
@@ -1,5 +1,5 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 
 class SampleTest(StaticLiveServerTestCase):
@@ -7,12 +7,29 @@ class SampleTest(StaticLiveServerTestCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.selenium = webdriver.Firefox(executable_path = '/usr/local/bin/geckodriver')
-        cls.selenium.implicitly_wait(10)
 
     @classmethod
     def tearDownClass(cls):
         cls.selenium.quit()
         super().tearDownClass()
 
-    def test_00_login(self):
+    # Lozza has heard about this new sick app, called viivote.com. Out of curiosity
+    # Lozza goes away and checked it out
+    def test_00_client_goes_to_site_and_try_to_log_in(self):
         self.selenium.get('%s%s' % (self.live_server_url, '/main/'))
+        # Lozza noticed that title and header mention viivote
+        self.assertIn('viiVote', self.selenium.title)
+
+        # Lozza thinks that is a member, because he is confused, he decided to enter his username and password
+        self.selenium.find_element_by_xpath('//input[@name="username"]').send_keys('lozza199')
+        self.selenium.find_element_by_xpath('//input[@name="password"]').send_keys('qazwsxedcrfv')
+        self.selenium.find_element_by_xpath('//button[@class]').click()
+        self.fail('Finish the test!')
+
+        
+
+
+
+
+if __name__ == '__main__':
+    unittest.main(warning = 'ignore')
