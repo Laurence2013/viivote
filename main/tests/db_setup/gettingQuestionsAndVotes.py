b08d68e1e475cd6c89b80ca2fccd7b9bc566103c
Laurence2013 <djroa1985@gmail.com>
2019-02-09 23:57:51 +0000

    7th 7-7s commit

--- main/tests/db_setup/gettingQuestionsAndVotes.py
+++ main/tests/db_setup/gettingQuestionsAndVotes.py
@@ -2,13 +2,16 @@ from main.models import *
 
 class GettingQuestionsAndVotes:
     def __init__(self, **kwargs):
-        self.__qs = kwargs.get('question')
+        self.__qs       = kwargs.get('question')
+        self.__vote_a   = kwargs.get('vote_a')
+        self.__vote_b   = kwargs.get('vote_b')
+        self.__vote_c   = kwargs.get('vote_c')
 
     def questions(self):
         Ask_A_Question_table.objects.create(question = self.__qs).save()
 
     def get_questions(self):
-        qs = Ask_A_Question_table.objects.values('id', 'question', 'date_updated')
+        qs = Ask_A_Question_table.objects.values('id', 'question', 'date_updated')[0]
         return qs
 
     def questions_votes_table(self):
@@ -18,10 +21,22 @@ class GettingQuestionsAndVotes:
         pass
 
     def vote_a_table(self):
-        pass
+        Vote_A_table.objects.create(vote = self.__vote_a, total_votes = 0).save()
 
+    def get_vote_a(self):
+        a = Vote_A_table.objects.values('id', 'vote', 'total_votes', 'date_updated')[0]
+        return a
+    
     def vote_b_table(self):
-        pass
+        Vote_B_table.objects.create(vote = self.__vote_b, total_votes = 0).save()
+    
+    def get_vote_b(self):
+        b = Vote_B_table.objects.values('id', 'vote', 'total_votes', 'date_updated')[0]
+        return b
 
     def vote_c_table(self):
-        pass
+        Vote_C_table.objects.create(vote = self.__vote_c, total_votes = 0).save()
+    
+    def get_vote_c(self):
+        c = Vote_C_table.objects.values('id', 'vote', 'total_votes', 'date_updated')[0]
+        return c
--- main/tests/unit_tests/gettingNumberOfVotesForUserTests.py
+++ main/tests/unit_tests/gettingNumberOfVotesForUserTests.py
@@ -8,11 +8,25 @@ class GettingNumberOfVotesForUserTests(TestCase):
     def setUp(self):
         self.user = UserLoginInfo('lozza2000','lozza2000@gmail.com')
         self.user.setupLogin() 
-        self.question1 = GettingQuestionsAndVotes(question = 'lorem 00')
+        self.question1 = GettingQuestionsAndVotes(question = 'lorem 00', vote_a = 'lorem 00 vote a', vote_b = 'lorem 00 vote b', vote_c = 'lorem 00 vote c')
         self.question1.questions()
+        self.question1.vote_a_table()
+        self.question1.vote_b_table()
+        self.question1.vote_c_table()
 
-    def test_00_sample(self):
-        test = self.question1.get_questions()
-        print(test)
-        self.assertTrue(True)
+    def test_00_a_question_is_the_same(self):
+        qs = self.question1.get_questions()
+        self.assertEqual('lorem 00', qs.get('question'))
+
+    def test_01_vote_a_is_the_same(self):
+        a = self.question1.get_vote_a()
+        self.assertEqual('lorem 00 vote a', a.get('vote'))
+    
+    def test_02_vote_b_is_the_same(self):
+        b = self.question1.get_vote_b()
+        self.assertEqual('lorem 00 vote b', b.get('vote'))
+    
+    def test_03_vote_c_is_the_same(self):
+        c = self.question1.get_vote_c()
+        self.assertEqual('lorem 00 vote c', c.get('vote'))
 
